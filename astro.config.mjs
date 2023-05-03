dfa0b11501c502b4d795dd8f682bc6ef89eccec1
Ronan <spacechickenrobot@gmail.com>
2023-05-03 14:22:00 +0000

    config astro for git pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://team-701-gonk-squad.github.io',
+  base: '/team-701-gonk-squad.github.io',
+})
