7d9246d92a595877c9e3d78d0786ee9080f0329a
Ronan <spacechickenrobot@gmail.com>
2023-11-17 13:36:34 +0000

    initial commit - color fade on scroll, empty header nav

---
+++ src/App.tsx
@@ -0,0 +1,94 @@
+import "./App.css";
+import "./fonts/SFDistantGalaxy.ttf";
+import { useEffect } from 'react';
+
+
+function App() {
+  useEffect(() => {
+    const handleScroll = () => {
+      document.documentElement.style.setProperty('--hero-color-1', 'rgb(0, 110, 120)');
+      document.documentElement.style.setProperty('--hero-color-2', 'rgb(92, 0, 0)');
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "96vw",
+          height: "100vh",
+          alignSelf: "center",
+          justifySelf: "center",
+        }}
+        className="hero"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignContent: "center",
+            justifyContent: "center",
+            marginLeft: "200px",
+            marginRight: "200px",
+            alignSelf: "start",
+            minHeight: "90vh",
+          }}
+        >
+          <h1 className="star-wars">Gonk Squad</h1>
+          <img className="gonk" src="./GonkWhite.png" />
+        </div>
+      </div>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "sticky",
+          top: 0,
+          backgroundColor: "black",
+          width: "96vw",
+          paddingTop: "10px",
+        }}
+      >
+        <nav className="nav">
+          <ul
+            style={{
+              listStyleType: "none",
+              display: "flex",
+              justifyContent: "space-between",
+              width: "80vw",
+              alignItems: "center",
+              margin: 0,
+              padding: 0,
+            }}
+          >
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#about">About Us</a>
+            </li>
+            <li>
+              <a href="#contact">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div
+        style={{
+          backgroundColor: "black",
+          width: "96vw",
+          height: "300vh",
+        }}
+      ></div>
+    </div>
+  );
+}
+
+export default App;
