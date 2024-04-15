6209422f52991a8ed76ff33b04d5a722fe329ec6
Ronan <spacechickenrobot@gmail.com>
2024-04-15 17:32:27 -0400

    change yt link

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 // import TypingParagraph from "./typingparagraph";
 // import Events from "./events.tsx";
 // import CodeBlock from "./codeblock.tsx";
-
+//hello:)
 function App() {
   useEffect(() => {
     const handleScroll = () => {
@@ -38,142 +38,160 @@ function App() {
 
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "96vw",
-          height: "100vh",
-          alignSelf: "center",
-          justifySelf: "center",
-        }}
-        className="hero"
-      >
-        <div className="herocase">
-          <h1 className="star-wars">Gonk Squad</h1>
-          <img className="gonk" src="./GonkWhite.png" />
+      <div>
+        <div className="hero">
+          <div className="herocase">
+            <h1 className="star-wars">gonk squad</h1>
+            <img
+              className="gonk"
+              src="./GonkWhite.png"
+              alt="Gonk droid image"
+            />
+          </div>
         </div>
-      </div>
-      <header
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          position: "sticky",
-          top: 0,
-          bottom: 0,
-          width: "100%",
-        }}
-      >
-        <nav className="nav">
+        <header
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "sticky",
+            top: 0,
+            bottom: 0,
+            width: "100%",
+          }}
+        >
+          <nav className="nav">
+            <ul
+              className="navlist"
+              style={{
+                listStyleType: "none",
+                display: "flex",
+                justifyContent: "space-between",
+                width: "90vw",
+                alignItems: "center",
+                margin: 0,
+              }}
+            >
+              <li>
+                <a href="http://localhost:5173/#top" className="gonk-logo">
+                  <img
+                    src="./GonkWhite.png"
+                    className="gonk-logo"
+                    alt="Gonk droid logo"
+                  />
+                </a>
+              </li>
+              <li>
+                <h1 className="nav-text">team 701 gonk squad</h1>
+              </li>
+              {/* Uncomment and use these if needed
+        <li><a className="nav-text" href="#events">Events</a></li>
+        <li><a className="nav-text" href="#gallery">Gallery</a></li>
+        <li><a className="nav-text" href="#about">About Us</a></li>
+        <li><a className="nav-text" href="#blog">Blog</a></li>
+        <li><a className="nav-text" href="#contact">Contact Us</a></li>
+        */}
+            </ul>
+          </nav>
+        </header>
+        <img style={{ width: "100%" }} src="/teamcrop.jpg" alt="Team image" />
+        <div className="outreach">outreach</div>
+        <div className="aboutTheRobot">about the robot</div>
+        <div className="programming">programming</div>
+        <footer
+          style={{
+            listStyleType: "none",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+          }}
+        >
           <ul
             style={{
               listStyleType: "none",
               display: "flex",
               justifyContent: "space-between",
-              width: "90vw",
               alignItems: "center",
-              margin: 0,
+              paddingLeft: "0px",
             }}
-            className="navlist"
           >
-            <a href="http://localhost:5173/#top" className="gonk-logo">
-              <img src="./GonkWhite.png" className="gonk-logo" />
-            </a>
             <li>
-              <a className="nav-text" href="#events">
-                Events
+              <a href="https://github.com/orgs/gonksquad/">
+                <img
+                  className="social-icon"
+                  src="./github.png"
+                  alt="GitHub icon"
+                />
               </a>
             </li>
             <li>
-              <a className="nav-text" href="#gallery">
-                Gallery
+              <a href="https://www.tiktok.com/@gonksquad_701">
+                <img
+                  className="social-icon"
+                  src="./tiktok.png"
+                  alt="TikTok icon"
+                />
               </a>
             </li>
             <li>
-              <a className="nav-text" href="#about">
-                About Us
+              <a href="https://www.youtube.com/@701gonkatrucks9?">
+                <img
+                  className="social-icon"
+                  src="./youtube.png"
+                  alt="YouTube icon"
+                />
               </a>
             </li>
             <li>
-              <a className="nav-text" href="#blog">
-                Blog
-              </a>
-            </li>
-            <li>
-              <a className="nav-text" href="#contact">
-                Contact Us
+              <a href="https://www.facebook.com/ftcteam701/">
+                <img
+                  className="social-icon"
+                  src="./facebook.png"
+                  alt="Facebook icon"
+                />
               </a>
             </li>
           </ul>
-        </nav>
-      </header>
-      {/* <div
-        style={{
-          backgroundColor: "black",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <Section
-          sectionimage="url('/team.png')"
-          text="Team 701"
-          id="team701"
-        ></Section>
-      </div> */}
-
-      {/* <TypingParagraph /> */}
-
-      <img style={{ width: "100%" }} src="/teamcrop.jpg" alt="" />
-
-      {/* <Events>
-      </Events> */}
-      <footer
-        style={{
-          listStyleType: "none",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <ul
-          style={{
-            listStyleType: "none",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            paddingLeft: "0px",
-          }}
-        >
           <li>
-            <a href="https://github.com/orgs/gonksquad/">
-              <img className="social-icon" src="./github.png" />
+            Social Media Icons Designed by{" "}
+            <a className="social-link" href="https://www.freepik.com">
+              Freepik
             </a>
           </li>
+        </footer>
+      </div>
+      <div>
+        <ul>
           <li>
-            <a href="https://www.tiktok.com/@gonksquad_701">
-              <img className="social-icon" src="./tiktok.png" />
-            </a>
+            Outreach
+            <ul>
+              <li>Sponsors</li>
+              <li>Industry Connections</li>
+              <li>Goals</li>
+              <li>Events</li>
+              <li>Conclusion</li>
+            </ul>
           </li>
           <li>
-            <a href="https://www.youtube.com/channel/UCxxZ-aPJ2NvUTdGRI2oKzDg">
-              <img className="social-icon" src="./youtube.png" />
-            </a>
+            About the Robot
+            <ul>
+              <li>Design Process</li>
+              <li>Overview</li>
+              <li>Subsystems</li>
+              <li>Strategy</li>
+            </ul>
           </li>
           <li>
-            <a href="https://www.facebook.com/ftcteam701/">
-              <img className="social-icon" src="./facebook.png" />
-            </a>
+            Programming
+            <ul>
+              <li>LED Indicators</li>
+              <li>Autonomous</li>
+              <li>Odometry</li>
+              <li>Vision</li>
+            </ul>
           </li>
         </ul>
-        <li>
-          Social Media Icons Designed by {""}
-          <a className="social-link" href="https://www.freepik.com">
-            Freepik
-          </a>
-        </li>
-      </footer>
+      </div>
     </div>
   );
 }
